b33177a5e8234e49d1e7ae64abf9f194753f6174
dubois15 <104189680+dubois15@users.noreply.github.com>
2022-05-13 15:26:59 +0900

    modif

--- script.js
+++ script.js
@@ -2,12 +2,23 @@ x=650;
 y=365;
 dx=5;
 dy=5;
+
+largeur=document.getElementById("terrain").style.width
+hauteur=document.getElementById("terrain").style.height
+rayon=document.getElementById("balle").style.height
+console.log(largeur,hauteur,rayon)
+
 function deplacement(){
 document.getElementById("balle").style.left=x+"px"
 document.getElementById("balle").style.top=y+"px"
+if (x + dx > largeur-rayon || x + dx < rayon) {
+        dx = -dx;
+    }
+if(y + dy > hauteur-rayon || y + dy < rayon) {
+        dy = -dy;
+    }
 x += dx;
 y += dy;
-console.log(x,y)
 }
 setInterval(deplacement,20)
 
@@ -26,3 +37,5 @@ function deplacement_racket2(u){
 		u=0	
 	document.getElementById("racket2").style.top=u+"px"	
 }
+
+
