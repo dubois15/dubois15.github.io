8cbb2e2a4e6a14c6affd53b7ae262bf58ce24969
dubois15 <104189680+dubois15@users.noreply.github.com>
2022-05-13 14:34:56 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+x=650;
+y=365;
+dx=5;
+dy=5;
 function deplacement(x,y){
-document.getElementById("balle").style.
+document.getElementById("balle").style.left=x+"px"
+document.getElementById("balle").style.top=y+"px"
+x += dx;
+y += dy;
+console.log(x,y)
 }
+setInterval(deplacement,20)
