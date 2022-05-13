83bd2c3325dd27473b0158ca2dc3e4b28fd78c2a
dubois15 <104189680+dubois15@users.noreply.github.com>
2022-05-13 15:07:05 +0900

    mise a jour
    mise a jour

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ x=650;
 y=365;
 dx=5;
 dy=5;
-function deplacement(x,y){
+function deplacement(){
 document.getElementById("balle").style.left=x+"px"
 document.getElementById("balle").style.top=y+"px"
 x += dx;
@@ -10,3 +10,19 @@ y += dy;
 console.log(x,y)
 }
 setInterval(deplacement,20)
+
+function deplacement_racket1(u){
+	if (u>300)
+		u=300
+	if (u<0)
+		u=0	
+	document.getElementById("racket1").style.top=u+"px"	
+}
+
+function deplacement_racket2(u){
+	if (u>300)
+		u=300
+	if (u<0)
+		u=0	
+	document.getElementById("racket2").style.top=u+"px"	
+}
